6d1e0703715311db774f52413d09f9d150670a79
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-18 15:15:28 +0100

    Add methods for square & circle objects that take additional arguments other than the object dictionary itself
    Modify call function to accept variable number of argumentsQ

--- ch2/shape_dict.py
+++ ch2/shape_dict.py
@@ -6,9 +6,13 @@ def square_perimeter(thing):
 def square_area(thing):
     return thing["side"] ** 2
 
+def square_larger(thing, size):
+    return call(thing, "area") > size
+
 Square = {
     "perimeter": square_perimeter,
     "area": square_area,
+    "larger": square_larger,
     "_classname": "Square"
 }
 
@@ -25,9 +29,13 @@ def circle_perimeter(thing):
 def circle_area(thing):
     return math.pi * thing["radius"] ** 2
 
+def circle_larger(thing, size):
+    return call(thing, "area") > size
+
 Circle = {
     "perimeter": circle_perimeter,
     "area": circle_area,
+    "larger": circle_larger,
     "_classname": "Circle "
 }
 def circle_new(name, radius):
@@ -37,8 +45,8 @@ def circle_new(name, radius):
         "_class": Circle
     }
 
-def call(thing, method_name):
-    return thing["_class"][method_name](thing)
+def call(thing, method_name, *args, **kwargs):
+    return thing["_class"][method_name](thing, *args, **kwargs)
 
 examples = [square_new("sq", 3), circle_new("ci", 2)]
 for ex in examples:
@@ -47,3 +55,5 @@ for ex in examples:
     a = call(ex, "area")
     c = ex["_class"]["_classname"]
     print(f"{n} is a {c}: {p:.2f} {a:.2f}")
+    result = call(ex, "larger", 10)
+    print(f"is ex['name'] larger? {result}")
