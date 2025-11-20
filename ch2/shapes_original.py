56362d3af77f655b383ba07a1546a7b6ab358cfe
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-20 21:24:45 +0100

    Objects, classes, methods,Inheritance and contructor methods implemented using actual the python class notation

---
+++ ch2/shapes_original.py
@@ -0,0 +1,44 @@
+import math
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    def perimeter(self):
+        raise NotImplementedError("perimeter")
+    def area(self):
+        raise NotImplementedError("area")
+
+    def density(self, weight):
+        return weight / self.area()
+
+class Square(Shape):
+    def __init__(self, name, side):
+        super().__init__(name)
+        self.side = side
+
+    def perimeter(self):
+        return 4 * self.side
+    def area(self):
+        return self.side ** 2
+
+class Circle(Shape):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    def area(self):
+        return math.pi * self.radius ** 2 
+
+examples = [Square("sq", 3), Circle("ci", 2)]
+
+# import pdb;pdb.set_trace()
+for thing in examples:
+    n = thing.name
+    p = thing.perimeter()
+    a = thing.area()
+    d = thing.density(5)
+    print(f"{n} has perimeter {p:.2f}, area {a:.2f} and density {d:.2f}")
+
