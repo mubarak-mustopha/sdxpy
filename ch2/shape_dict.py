880bec25442d2d70b194e38ea24205abe9aee024
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-20 20:50:13 +0100

    implement class inheritance using dicts

--- ch2/shape_dict.py
+++ ch2/shape_dict.py
@@ -1,5 +1,14 @@
 import math
 
+def density(thing, weight):
+    return weight / call(thing, "area")
+
+Shape = {
+    "density": density,
+    "_classname": "Shape",
+    "_parent": None
+}
+
 def square_perimeter(thing):
     return 4 * thing["side"]
 
@@ -13,7 +22,8 @@ Square = {
     "perimeter": square_perimeter,
     "area": square_area,
     "larger": square_larger,
-    "_classname": "Square"
+    "_classname": "Square",
+    "_parent": Shape
 }
 
 def square_new(name, side):
@@ -36,7 +46,8 @@ Circle = {
     "perimeter": circle_perimeter,
     "area": circle_area,
     "larger": circle_larger,
-    "_classname": "Circle "
+    "_classname": "Circle ",
+    "_parent": Shape
 }
 def circle_new(name, radius):
     return {
@@ -46,7 +57,16 @@ def circle_new(name, radius):
     }
 
 def call(thing, method_name, *args, **kwargs):
-    return thing["_class"][method_name](thing, *args, **kwargs)
+    method = find(thing["_class"], method_name)
+    return method(thing, *args, **kwargs)
+
+def find(cls, method_name):
+    while cls is not None:
+        if method_name in cls:
+            return cls[method_name]
+        cls = cls["_parent"]
+    raise NotImplementedError(method_name)
+
 
 examples = [square_new("sq", 3), circle_new("ci", 2)]
 for ex in examples:
@@ -54,6 +74,7 @@ for ex in examples:
     p = call(ex, "perimeter")
     a = call(ex, "area")
     c = ex["_class"]["_classname"]
-    print(f"{n} is a {c}: {p:.2f} {a:.2f}")
+    d = call(ex, "density", 5)
+    print(f"{n} is a {c}: {p:.2f} {a:.2f} with density {d:.2f}")
     result = call(ex, "larger", 10)
-    print(f"is ex['name'] larger? {result}")
+    print(f"is {ex['name']} larger? {result}")
