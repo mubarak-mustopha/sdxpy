8b5ae99104d6207ed3d56c1843a64a5e0ee4d1b0
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-24 13:08:36 +0100

    Add failing test for /{a,b}/ `either` matcher 	* Either matcher currently fails because it doesn't 	  utilize the `rest` matcher when matching

--- ch4/test_glob_either.py
+++ ch4/test_glob_either.py
@@ -6,5 +6,13 @@ def test_either_two_literals_first():
     assert Either(Lit("a"), Lit("b")).match("a")
 
 def test_either_two_literals_not_both():
-    # /{a,b}/ doesn't matches "ab"
+    # /{a,b}/ doesn't matche "ab"
     assert not Either(Lit("a"), Lit("b")).match("ab")
+
+def test_either_followed_by_literal_match():
+    # /{a,b}c/ matches "ac"
+    assert Either(Lit("a"), Lit("b"), Lit("c")).match("ac")
+
+def test_either_followed_by_literal_no_match():
+    # /{a,b}c/ doesn't matches "ax"
+    assert not Either(Lit("a"), Lit("b"), Lit("c")).match("ax")
